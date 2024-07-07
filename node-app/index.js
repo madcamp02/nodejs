9723e2db1e222b3441e7870b66a17eb103d71f21
jieunyy <ina011011@naver.com>
2024-07-07 19:21:59 +0900

    feat: mysql db access

--- node-app/index.js
+++ node-app/index.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 3000;
 
 const db = mysql.createConnection({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'mydb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
